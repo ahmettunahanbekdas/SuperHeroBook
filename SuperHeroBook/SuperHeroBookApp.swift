98deac04295fd319a2168a6cc45b1173767f3ff0
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-08-08 14:58:47 +0300

    Initial Commit

---
+++ SuperHeroBook/SuperHeroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroBookApp.swift
+//  SuperHeroBook
+//
+//  Created by Ahmet Tunahan Bekdaş on 8.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
