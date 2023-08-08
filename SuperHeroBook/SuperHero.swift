526154750599b89ad9f016d08bf2545c1c1999d8
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-08-08 16:17:45 +0300

    create model

---
+++ SuperHeroBook/SuperHero.swift
@@ -0,0 +1,52 @@
+//
+//  SuperHero.swift
+//  SuperHeroBook
+//
+//  Created by Ahmet Tunahan Bekdaş on 8.08.2023.
+//
+
+import Foundation
+
+import SwiftUI
+import CoreLocation
+ 
+struct Superhero : Identifiable{
+    var id = UUID()
+    var name : String
+    var realName : String
+    var imageName : String
+    var city : String
+    var job : String
+    var cordinate : Cordinate
+    
+    var cordinateLocation : CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: cordinate.latitude, longitude: cordinate.latitude)
+    }
+}
+
+struct Cordinate {
+    var latitude : Double
+    var longitude : Double
+}
+
+let batman = Superhero(name: "Batman", realName: "Bruce Vayne", imageName: "batman", city: "Gotham", job: "Buisnes Man", cordinate: Cordinate(latitude: 41.88803034201345, longitude: -87.71074893170525))
+
+let spiderman = Superhero(name: "Spiderman", realName: "Peter Parker", imageName: "spiderman", city: "New York", job: "Photographer", cordinate: Cordinate(latitude: 40.72703794724936, longitude: -73.9989177642422))
+
+let superman = Superhero(name: "Superman", realName: "Clark Kent", imageName: "superman", city: "Metropolis", job: "Journalist", cordinate: Cordinate(latitude: 37.16050659768459, longitude: -88.72775820067034))
+
+let ironMan = Superhero(name: "Iron Man", realName: "Tony Stark", imageName: "ironman", city: "Malibu", job: "Engineer", cordinate: Cordinate(latitude: 34.035374675003574, longitude: -118.6921683596528))
+
+let greenLantern = Superhero(name: "Green Lantern", realName: "Hal Jordan", imageName: "greenlantern", city: "Coast City", job: "Test Pilot", cordinate: Cordinate(latitude: 33.23111809226882, longitude: -117.3254458372367))
+
+let hulk = Superhero(name: "Hulk", realName: "Bruce Banner", imageName: "hulk", city: "New York", job: "Scientist", cordinate: Cordinate(latitude:40.72703794724936, longitude: -73.9989177642422))
+
+let aquaman = Superhero(name: "Aquaman", realName: "Arthur Curry", imageName: "aquaman", city: "Atlantis", job: "King of the Seas", cordinate: Cordinate(latitude: -27.250982858485166, longitude: -15.597790793084965))
+
+let flash = Superhero(name: "Flash", realName: "Barry Allen", imageName: "flash", city: "Central City", job: "Forensic Scientist", cordinate: Cordinate(latitude: 39.800512640833894, longitude: -105.5147838963449))
+
+let captainAmerica = Superhero(name: "Captain America", realName: "Steve Rogers", imageName: "captainamerica", city: "New York", job: "Super Soldier", cordinate: Cordinate(latitude: 40.72703794724936, longitude: -73.9989177642422))
+
+
+let superHeroArray = [batman, spiderman, superman, ironMan, greenLantern, hulk, aquaman, flash, captainAmerica]
+
